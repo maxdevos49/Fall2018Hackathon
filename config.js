4c4273d3d8676d150f95559830379b7c7c91c040
Joseph Naberhaus <naberhausj@gmail.com>
2018-10-27 18:10:50 -0500

    Added example photos

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports =  {
     "name": "Hackathon 2018 API Server",
     "apiVersion": "1.0.0",
     "owner":"The Best Hackathon Team Ever!!! And mason because he is here in spirit",
-    "dbUrl": process.env.MONGODB_URI || "mongodb://hackathonGuy:1Password@ds259820.mlab.com:59820/fallhackathon2018",
+    "dbUrl": process.env.MONGODB_URI || "mongodb://Test:password1@ds143683.mlab.com:43683/photocenter",
     "path": __dirname
 };
