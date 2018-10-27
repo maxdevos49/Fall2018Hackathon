cb43c99129e4cb2beecf7c41a7b66c1dc5ad398e
Joseph Naberhaus <naberhausj@gmail.com>
2018-10-27 18:29:37 -0500

    Changed config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports =  {
     "name": "Hackathon 2018 API Server",
     "apiVersion": "1.0.0",
     "owner":"The Best Hackathon Team Ever!!! And mason because he is here in spirit",
-    "dbUrl": process.env.MONGODB_URI || "<YOUR MONGODB URL",
+    "dbUrl": process.env.MONGODB_URI || "<YOUR MONGODB URL>",
     "path": __dirname
 };
